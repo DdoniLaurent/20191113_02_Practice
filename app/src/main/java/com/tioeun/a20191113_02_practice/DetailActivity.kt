1704b41a36d9a62b037b1bc6060c5a45a6b3852d
YongMinSun <ddoni1010@gmail.com>
2019-11-13 22:16:49 +0900

    연습문제 완성!!

--- app/src/main/java/com/tioeun/a20191113_02_practice/DetailActivity.kt
+++ app/src/main/java/com/tioeun/a20191113_02_practice/DetailActivity.kt
@@ -1,8 +1,14 @@
 package com.tioeun.a20191113_02_practice
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.tioeun.a20191113_02_practice.datas.PizzaData
 import kotlinx.android.synthetic.main.activity_detail.*
 
@@ -20,6 +26,31 @@ class DetailActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        callBtn.setOnClickListener {
+
+            var permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    var uri = Uri.parse("tel: ${phoneNum.text.toString().replace("-", "")}")
+                    var intent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(intent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 설정해야 전화 사용이 가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("전화 권한을 설정해야 바로 연결 가능합니다.")
+                .setDeniedMessage("설정 > 권한에서 허용 가능합니다.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
+
+        okBtn.setOnClickListener {
+            finish()
+        }
     }
 
     override fun setValues() {
