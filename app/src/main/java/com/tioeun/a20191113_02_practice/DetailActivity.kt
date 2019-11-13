7809864bb207416951ec56d2fbb2bdd61907c669
YongMinSun <ddoni1010@gmail.com>
2019-11-13 22:13:06 +0900

    상세 목록 표출 완성

--- app/src/main/java/com/tioeun/a20191113_02_practice/DetailActivity.kt
+++ app/src/main/java/com/tioeun/a20191113_02_practice/DetailActivity.kt
@@ -2,8 +2,13 @@ package com.tioeun.a20191113_02_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.tioeun.a20191113_02_practice.datas.PizzaData
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : BaseActivity() {
+
+    var pizzaData:PizzaData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +23,12 @@ class DetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        pizzaData = intent.getSerializableExtra("pizzaData") as PizzaData
 
+        Glide.with(mContext).load("${pizzaData?.logoUrl}").into(pizzaLogo)
+
+        storeName.text = pizzaData?.name
+        phoneNum.text = pizzaData?.phoneNum
     }
 
 
